6b7821788595aac704e0513ae3e54c8952ed1050
LarssonDev <Larssonleonards@gmail.com>
2024-07-10 17:52:53 +0530

    Updated dist directory with new files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/QR-Attendance-Presenter/', // Set the correct base path for deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@babel/runtime']
